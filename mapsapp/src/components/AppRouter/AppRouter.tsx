7010abea99817ebf1d86499c4e0a217ed5908772
Sun Lama <isamo.agverdiev@gmail.com>
2022-01-07 02:27:39 +0300

    refactor: routes

--- mapsapp/src/components/AppRouter/AppRouter.tsx
+++ mapsapp/src/components/AppRouter/AppRouter.tsx
@@ -1,22 +1,28 @@
 import * as React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { privateRoutes, publicRoutes } from '../../router/Routes';
 import { isAuth } from '../../App';
+import Home from '../../pages/Home';
+import Main from '../../pages/Main';
+import SignUp from './../auth/SignUp';
+import SignIn from './../auth/SignIn';
+import Forgot from '../auth/Forgot';
 
 const AppRouter: React.FC = () => {
   return isAuth ? (
     <Routes>
-      {privateRoutes.map(route => (
-        <Route path={route.path} element={<route.element />} key={route.path} />
-      ))}
+      <Route path='*' element={<Main />} />
+      <Route path='/main' element={<Main />} />
+      <Route path='/sign_up' element={<SignUp />} />
     </Routes>
   ) : (
     <Routes>
-      {publicRoutes.map(route => (
-        <Route path={route.path} element={<route.element />} key={route.path} />
-      ))}
+      <Route path='*' element={<Home />} />
+      <Route path='/home' element={<Home />} />
+      <Route path='/main' element={<Main />} />
+      <Route path='/sign_up' element={<SignUp />} />
+      <Route path='/forgot' element={<Forgot />} />
+      <Route path='/sign_in' element={<SignIn />} />
     </Routes>
   );
 };
-
 export default AppRouter;
