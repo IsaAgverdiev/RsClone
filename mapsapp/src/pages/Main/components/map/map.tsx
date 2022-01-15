c569620ec338e5487b0f16067fb39cfa2134c700
makhitr <ring_ru@yahoo.com>
2022-01-15 12:59:25 +0300

    feat: add map sidebar

--- mapsapp/src/pages/Main/components/map/map.tsx
+++ mapsapp/src/pages/Main/components/map/map.tsx
@@ -2,12 +2,12 @@ import React, { useState, useRef, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import './Map.scss'
+import { MapState } from "../../../../store/reducers/mapReducer";
 
 mapboxgl.accessToken = "pk.eyJ1IjoibWFraGl0ciIsImEiOiJja3h4a3ViNGMwamd5Mm9ycTB2NjM5ZGhjIn0.ZLAA9nNM-a2DTiWN1YrGHQ"
 
 
 const Map = () => {
-
   const [map, setMap] = useState<mapboxgl.Map>();
   const mapNode = useRef(null);
   const [lng, setLng] = useState(-70.9);
@@ -26,15 +26,37 @@ const Map = () => {
 
     setMap(mapboxMap);
 
+    if (mapboxMap) {
+      mapboxMap.on('move', () => {
+        setLng(+(mapboxMap.getCenter().lng.toFixed(4)));
+        setLat(+(mapboxMap.getCenter().lat.toFixed(4)));
+        setZoom(+(mapboxMap.getZoom().toFixed(2)));
+      })
+
+    }
+
     return () => {
       mapboxMap.remove();
     };
+
+    // useEffect(() => {
+    //   if (!map.current) return; // wait for map to initialize
+    //   map.current.on('move', () => {
+    //   setLng(map.current.getCenter().lng.toFixed(4));
+    //   setLat(map.current.getCenter().lat.toFixed(4));
+    //   setZoom(map.current.getZoom().toFixed(2));
+    //   });
+    //   });
   }, []);
 
   return (
+    <>
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapNode} className="map-container" />
 
-    <div ref={mapNode} className="map-container" />
-
+    </>
   )
 }
 
