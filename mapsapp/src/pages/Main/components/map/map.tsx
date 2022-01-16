8774d84b94ab8c0e13ecbfecc1e82144916b254a
makhitr <ring_ru@yahoo.com>
2022-01-16 16:03:37 +0300

    feat: add map markers

--- mapsapp/src/pages/Main/components/map/map.tsx
+++ mapsapp/src/pages/Main/components/map/map.tsx
@@ -2,17 +2,15 @@ import React, { useState, useRef, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import './Map.scss'
-import { MapState } from "../../../../store/reducers/mapReducer";
 
 mapboxgl.accessToken = "pk.eyJ1IjoibWFraGl0ciIsImEiOiJja3h4a3ViNGMwamd5Mm9ycTB2NjM5ZGhjIn0.ZLAA9nNM-a2DTiWN1YrGHQ"
-
 
 const Map = () => {
   const [map, setMap] = useState<mapboxgl.Map>();
   const mapNode = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
+  const [lng, setLng] = useState(37.60);
+  const [lat, setLat] = useState(55.73);
+  const [zoom, setZoom] = useState(10);
 
   useEffect(() => {
     const node = mapNode.current;
@@ -21,7 +19,7 @@ const Map = () => {
       container: node,
       style: "mapbox://styles/mapbox/streets-v11",
       center: [lng, lat],
-      zoom: zoom
+      zoom: zoom,
     });
 
     setMap(mapboxMap);
@@ -32,21 +30,20 @@ const Map = () => {
         setLat(+(mapboxMap.getCenter().lat.toFixed(4)));
         setZoom(+(mapboxMap.getZoom().toFixed(2)));
       })
-
     }
+
+    const marker1 = new mapboxgl.Marker()
+      .setLngLat([37.62, 55.75])
+      .addTo(mapboxMap);
+
+    const marker2 = new mapboxgl.Marker({ color: 'black', rotation: 45, draggable: true })
+      .setLngLat([37.72, 55.75])
+      .addTo(mapboxMap);
 
     return () => {
       mapboxMap.remove();
     };
 
-    // useEffect(() => {
-    //   if (!map.current) return; // wait for map to initialize
-    //   map.current.on('move', () => {
-    //   setLng(map.current.getCenter().lng.toFixed(4));
-    //   setLat(map.current.getCenter().lat.toFixed(4));
-    //   setZoom(map.current.getZoom().toFixed(2));
-    //   });
-    //   });
   }, []);
 
   return (
@@ -55,7 +52,6 @@ const Map = () => {
         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
       </div>
       <div ref={mapNode} className="map-container" />
-
     </>
   )
 }
