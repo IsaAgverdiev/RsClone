608be06d5572228a09510057c1e6967e74830f5c
Mariya N <kingfisher.str@gmail.com>
2022-01-18 21:58:24 +0500

    fix:fix

--- mapsapp/src/components/AppRouter/AppRouter.tsx
+++ mapsapp/src/components/AppRouter/AppRouter.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from '../../pages/Home';
-import Profile from '../../pages/Profile';
-import Settings from '../../pages/Settings';
 import Feature1 from '../../pages/features/Feature1';
 import Feature2 from '../../pages/features/Feature2';
 import Feature3 from '../../pages/features/Feature3';
+import Profile from '../../pages/Profile';
+import Settings from '../../pages/Settings';
 import SignUp from '../Auth/SignUp';
 import SignIn from '../Auth/SignIn';
 import Forgot from '../Auth/Forgot';
@@ -20,24 +20,16 @@ const AppRouter: React.FC<AppRouterProps> = ({ authorized, name }) => {
   return authorized ? (
     <Routes>
       <Route path='/home' element={<Home />} />
-<<<<<<< HEAD
+      <Route path='/sign_up' element={<SignUp />} />
       <Route path='main' element={<Main />} >
         <Route index element={<Map />}/>
         <Route path="Feature1" element={<Feature1 />} />
         <Route path="Feature2" element={<Feature2 />} />
         <Route path="Feature3" element={<Feature3 />} />
+        <Route path='/main/profile' element={<Profile />} />
+        <Route path='/main/settings' element={<Settings />} />
       </Route>
-      <Route path='/main/profile' element={<Profile />} />
-      <Route path='/main/settings' element={<Settings />} />
-      <Route path='/sign_up' element={<SignUp />} />
-=======
-      <Route path='/main' element={<PersistentDrawerLeft />}></Route>
->>>>>>> 522952f6ccf79a3f980e3084a7fa6f062f6292e1
 
-      <Route path='/sign_up' element={<SignUp />} />
-      <Route path='main/Feature1' element={<Feature1 />} />
-      <Route path='main/Feature2' element={<Feature2 />} />
-      <Route path='main/Feature3' element={<Feature3 />} />
       <Route path='*' element={<Navigate to='/main' />} />
     </Routes>
   ) : (
@@ -51,3 +43,7 @@ const AppRouter: React.FC<AppRouterProps> = ({ authorized, name }) => {
   );
 };
 export default AppRouter;
+
+
+
+
